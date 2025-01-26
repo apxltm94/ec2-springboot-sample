4de1dd4781822a54dc95161bd2835dd079ef237d
1reps <kdm9982@gmail.com>
2025-01-26 19:04:06 +0900

    sample first commit

---
+++ src/main/java/me/sample/Ec2SpringbootSampleApplication.java
@@ -0,0 +1,13 @@
+package me.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec2SpringbootSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ec2SpringbootSampleApplication.class, args);
+    }
+
+}
