dcbd717ab93ca83aa2f87f90a2614387297bfb51
1reps <kdm9982@gmail.com>
2025-01-27 16:47:26 +0900

    GET root

--- src/main/java/me/sample/core/sample/SampleController.java
+++ src/main/java/me/sample/core/sample/SampleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
-    @GetMapping("/v1/api/sample")
+    @GetMapping("/")
     public String sample() {
         return "sample";
     }
