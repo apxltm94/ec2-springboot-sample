5dac77157aae71677123a4a2be89ef8e92398441
1reps <kdm9982@gmail.com>
2025-02-01 20:19:52 +0900

    Spring Boot, MySQL, Redis 컨테이너 동시에 띄워보기

--- src/main/java/me/sample/core/sample/SampleController.java
+++ src/main/java/me/sample/core/sample/SampleController.java
@@ -1,13 +1,19 @@
 package me.sample.core.sample;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class SampleController {
 
+    private final RedisTemplate<String, Object> redisTemplate;
+
     @GetMapping("/")
     public String sample() {
+        redisTemplate.opsForValue().set("abc", "def");
         return "sample";
     }
 
