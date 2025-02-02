4f37bfa7a269e395a9da78334e83d000f48f0bd1
1reps <kdm9982@gmail.com>
2025-02-02 19:04:20 +0900

    local, dev 환경 분리

--- src/main/java/me/sample/core/controller/BoardController.java
+++ src/main/java/me/sample/core/controller/BoardController.java
@@ -4,16 +4,19 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import me.sample.core.domain.Board;
 import me.sample.core.service.BoardService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("boards")
+@RestController
 @RequiredArgsConstructor
 public class BoardController {
 
     private final BoardService boardService;
 
-    public List<Board> getBoards(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "10") int size) {
+    @GetMapping("/boards")
+    public List<Board> getBoards(@RequestParam(defaultValue = "1") int page,
+        @RequestParam(defaultValue = "10") int size) {
         return boardService.getBoards(page, size);
     }
 
