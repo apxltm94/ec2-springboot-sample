831ab9308eed1ebd620304c4237fa847a70d1058
1reps <kdm9982@gmail.com>
2025-02-02 19:42:24 +0900

    수정

--- src/main/java/me/sample/core/service/BoardService.java
+++ src/main/java/me/sample/core/service/BoardService.java
@@ -1,10 +1,27 @@
 package me.sample.core.service;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import me.sample.core.domain.Board;
+import me.sample.core.repository.BoardRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface BoardService {
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class BoardService {
 
-    List<Board> getBoards(int page, int size);
+    private final BoardRepository boardRepository;
+
+    @Transactional(readOnly = true)
+    public List<Board> getBoards(int page, int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        Page<Board> pageOfBoards = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
+        return pageOfBoards.getContent();
+    }
 
 }
